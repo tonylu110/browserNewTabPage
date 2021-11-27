4854caf365b1492973cbc17d633f43126c7c5925
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2021-11-27 14:21:39 +0800

    second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 function onkeywords() {
-    if(event.keyCode==13) {
-      var keywords = document.getElementById('keywords').value;
-      window.open('https://www.google.com/search?q='+keywords,'_self');
-    }
+  if(event.keyCode==13) {
+    var keywords = document.getElementById('keywords').value;
+    window.open('https://www.google.com/search?q='+keywords,'_self');
   }
+}
+
+function onBarButton(){
+  document.getElementById('bar').className = 'barSel';
+  document.getElementById('imgbefore').className = 'imgafter'
+}
+
+function onBarButtonBack(){
+  document.getElementById('bar').className = 'bar';
+  document.getElementById('imgbefore').className = 'imgbefore'
+}
+
+function onShow(){
+  document.getElementById('mainlink').style.display="";;
+  document.getElementById('button').hidden = false;
+  document.getElementById('history').hidden = false;
+  document.getElementById('reload').hidden = false;
+}
+
+function onHide(){
+  var hide = document.getElementById('button').hidden;
+  if(hide == false){
+    document.getElementById("mainlink").style.display="none"
+    document.getElementById('button').hidden = true;
+    document.getElementById('history').hidden = true;
+    document.getElementById('reload').hidden = true;
+  }else{
+    onShow();
+  }
+}
