228b2188a291328d0b49abbf9dd854d18f394aad
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2021-11-28 13:24:33 +0800

    change main.js's note

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-// This is onload script
+//This is onload script
+//test 'https://api.iyk0.com/ecy/api.php' can or not work
+//if it is working to load 'https://api.iyk0.com/ecy/api.php'
+//else load 'imgs/background.png'
 var i = new Image();
 i.src = 'https://api.iyk0.com/ecy/api.php';
 i.onload = function() {
@@ -13,55 +16,56 @@ i.onerror = function() {
 //This is searchBar function
 function onkeywords() {
   if(event.keyCode==13) {
-    var keywords = document.getElementById('keywords').value;
-    window.open('https://www.google.com/search?q='+keywords,'_self');
+    var keywords = document.getElementById('keywords').value; //get keywords
+    window.open('https://www.google.com/search?q='+keywords,'_self'); //to google search
   }
 }
 
 //This is historybar function
 function onBarButton(){
-  document.getElementById('bar').className = 'barSel';
-  document.getElementById('imgbefore').className = 'imgafter'
+  document.getElementById('bar').className = 'barSel'; //change historyBar style to barSelect style
+  document.getElementById('imgbefore').className = 'imgafter' //change historyBarIcon style to historyBarIconSelect style
 }
 
 //This is historybar back function
 function onBarButtonBack(){
-  document.getElementById('bar').className = 'bar';
-  document.getElementById('imgbefore').className = 'imgbefore'
+  document.getElementById('bar').className = 'bar'; //change hidtoryBar style to barBefore style
+  document.getElementById('imgbefore').className = 'imgbefore' //change historyBarIcon style to historyBayIconBefore style
 }
 
 //This is show all element function
 function onShow(){
-  document.getElementById('mainlink').style.display="";
-  document.getElementById('button').hidden = false;
-  document.getElementById('history').hidden = false;
-  document.getElementById('reload').hidden = false;
-  document.getElementById('cal').hidden = false;
-  document.getElementById('hide').hidden = false;
-  document.getElementById('searchbar').style.display="";
+  document.getElementById('mainlink').style.display=""; //show link element
+  document.getElementById('button').hidden = false; //show bottom button
+  document.getElementById('history').hidden = false; //show history button
+  document.getElementById('reload').hidden = false; //show reload button
+  document.getElementById('cal').hidden = false; // show calculator button
+  document.getElementById('hide').hidden = false; // show hide button
+  document.getElementById('searchbar').style.display=""; //show searchBar button
 }
 
 //This is hide all element function
 function onHide(){
-  document.getElementById("mainlink").style.display="none";
-  document.getElementById('button').hidden = true;
-  document.getElementById('history').hidden = true;
-  document.getElementById('reload').hidden = true;
-  document.getElementById('cal').hidden = true;
-  document.getElementById('hide').hidden = true;
-  document.getElementById('searchbar').style.display="none";
+  document.getElementById("mainlink").style.display="none"; //hide link elemnet
+  document.getElementById('button').hidden = true; //hide bottom button
+  document.getElementById('history').hidden = true; //hide history button
+  document.getElementById('reload').hidden = true; //hide reload button
+  document.getElementById('cal').hidden = true; //hide calculator button
+  document.getElementById('hide').hidden = true; //hide hide button
+  document.getElementById('searchbar').style.display="none"; //hide searchBar button
 }
 
 //This is hide all element function
+//if bottemButton is hide all element will hide
+//else show all element
 function onHideOrShow(){
   var hide = document.getElementById('button').hidden;
-  if(hide == false){
+  if(hide == false){ 
     onHide();
-    document.getElementById('hide').hidden = false;
-    document.getElementById('searchbar').style.display="";
+    document.getElementById('hide').hidden = false; //show hide button
+    document.getElementById('searchbar').style.display=""; //show searchBar element
   }else{
     onShow();
-    document.getElementById('searchbar').style.display="";
   }
 }
 
@@ -69,11 +73,11 @@ function onHideOrShow(){
 function onCal(){
   var hide = document.getElementById('button').hidden;
   if(hide == false){
-    document.getElementById('mainCal').hidden = false;
+    document.getElementById('mainCal').hidden = false; //show calculator element
     onHide();
-    document.getElementById('cal').hidden = false;
+    document.getElementById('cal').hidden = false; //show calculator button
   }else{
-    document.getElementById('mainCal').hidden = true;
+    document.getElementById('mainCal').hidden = true; //hide calculator element
     onShow();
   }
 }
