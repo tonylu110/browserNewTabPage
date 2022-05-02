0f43346ddaefb1f8c5683ac95f188b3c823bb328
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2022-05-02 15:14:44 +0800

    Add: one info window

--- src/components/One/One.jsx
+++ src/components/One/One.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import OneWindow from '../OneWindow/OneWindow'
 import './One.css'
 
 export default class One extends Component {
@@ -12,15 +13,30 @@ export default class One extends Component {
     this.state = {
       oneMain: {},
       oneButtonHeight: '',
-      oneButtonWidth: oneButtonWidth
+      oneButtonWidth: oneButtonWidth,
+      oneMainShow: false
     }
   }
   render() {
     return (
-      <div className='one_button' style={{marginBottom: this.state.oneButtonHeight, maxWidth: this.state.oneButtonWidth}}>
-          <span style={{maxWidth: this.state.oneButtonWidth}}>{this.state.oneMain.hitokoto}</span>
-      </div>
+      <>
+        <div className='one_button' style={{ marginBottom: this.state.oneButtonHeight, maxWidth: this.state.oneButtonWidth }} onClick={() => this.clickOneButton()}>
+          <span style={{ maxWidth: this.state.oneButtonWidth }}>{this.state.oneMain.hitokoto}</span>
+        </div>
+        <OneWindow oneMain={this.state.oneMain} oneMainShow={this.state.oneMainShow} event={
+          (e) => {
+            this.setState({
+              oneMainShow: e
+            })
+          }
+        } />
+      </>
     )
+  }
+  clickOneButton() {
+    this.setState({
+      oneMainShow: true
+    })
   }
   static getDerivedStateFromProps(props) {
     var screenwidth = window.innerWidth
---
+++ src/components/OneWindow/OneWindow.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import './OneWindow.css'
+
+export default class OneWindow extends Component {
+  constructor(props) {
+    super(props)
+    var screenwidth = window.innerWidth
+    var oneMainWidth = ''
+    var oneMainHeight = ''
+    var oneMainPadding = ''
+    var oneMainFontSize = ''
+    var oneMainBottm = '-170px'
+    if (screenwidth < 768) {
+      oneMainWidth = '270px'
+      oneMainHeight = '400px'
+      oneMainPadding = '40px'
+      oneMainFontSize = '30px'
+      oneMainBottm = '-50vh'
+    }
+    this.state = {
+      oneMain: {},
+      oneMainShow: false,
+      oneMainWidth: oneMainWidth,
+      oneMainHeight: oneMainHeight,
+      oneMainPadding: oneMainPadding,
+      oneMainFontSize: oneMainFontSize,
+      oneMainBottm: oneMainBottm
+    }
+  }
+  render() {
+    var screenHeight = document.documentElement.clientHeight
+    return (
+      <div className='one_main' style={{
+        bottom: (this.state.oneMainShow ? ((screenHeight - 402) / 2) + 'px' : this.state.oneMainBottm),
+        transform: (this.state.oneMainShow ? 'scale(1, 1)' : 'scale(0, 0)'),
+        width: this.state.oneMainWidth,
+        height: this.state.oneMainHeight,
+        padding: this.state.oneMainPadding
+      }}>
+        <div className='close_button' onClick={() => this.clickCloseButton()}>
+          <img src="img/close.png" alt='' />
+        </div>
+        <div className='one_main_info'>
+          <span className='one_num' style={{fontSize: this.state.oneMainFontSize}}>#{this.state.oneMain.id}</span>
+          <span className='one_text' style={{fontSize: this.state.oneMainFontSize}}>{this.state.oneMain.hitokoto}</span>
+          <span className='one_from' style={{fontSize: this.state.oneMainFontSize}}>———{this.state.oneMain.from}</span>
+        </div>
+      </div>
+    )
+  }
+  clickCloseButton() {
+    this.props.event(false)
+  }
+  static getDerivedStateFromProps(props) {
+    if (props.oneMain !== null) {
+      return {
+        oneMain: props.oneMain,
+        oneMainShow: props.oneMainShow
+      }
+    }
+    return null;
+  }
+}
