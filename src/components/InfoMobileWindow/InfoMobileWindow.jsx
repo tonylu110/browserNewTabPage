c5a88002bb17d69d23539cc7af64da5d9b4fd556
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2022-05-04 16:53:38 +0800

    Add: info main text

--- src/components/InfoMobileWindow/InfoMobileWindow.jsx
+++ src/components/InfoMobileWindow/InfoMobileWindow.jsx
@@ -12,8 +12,41 @@ export default class InfoMobileWindow extends Component {
         transform: this.state.infoWindowShow ? 'scale(1, 1)' : '',
         top: this.state.infoWindowShow ? ((screenHeight - 480) / 2) + 'px' : '-50vh',
       }}>
-        <div className='close_button' onClick={() => this.clickCloseButton()}>
+        <div className='close_button cl_mobile' onClick={() => this.clickCloseButton()}>
           <img src="img/close.png" alt='' />
+        </div>
+        <div className='info_inner'>
+          <div className='info_title'>
+            <span>关于 uyou 新标签页</span>
+            <img src="logo512.png" alt="" />
+          </div>
+          <div className='info_main'>
+            <div className="info_text">
+              这是基于 uyou 新标签页 chrome 扩展的网页版，她会让你感觉到和 chrome 扩展版几乎一样的体验，也可以让暂未支持的浏览器作为主页使用
+            </div>
+            <div className="info_text">
+              <div>这个新标签页的开源地址为：</div>
+              <a href="https://github.com/tonylu110/browserNewTabPage" target="view_window">点击前往</a>
+              <div style={{ marginTop: '15px' }}>如果你感兴趣也可以去这个项目的扩展版上给我一个 star，此标签页扩展的开源地址为：</div>
+              <a href="https://github.com/tonylu110/uyouNewTab" target="view_window">点击前往</a>
+            </div>
+            <div className="info_text">
+              <div>我的 GitHub 地址：</div>
+              <a href="https://github.com/tonylu110" target="view_window">点击前往</a>
+              <div className='mine_info_mobile'>
+                <div style={{ marginRight: '20px' }}>
+                  <div style={{ marginTop: '10px' }}>我的 酷安 账号：</div>
+                  <img src="img/coolapk.png" alt='' />
+                  <a href="http://www.coolapk.com/u/1126752" target="view_window">点击前往</a>
+                </div>
+                <div>
+                  <div style={{ marginTop: '10px' }}>欢迎加群讨论：</div>
+                  <img src="img/qq.jpg" alt='' />
+                  <a href="https://jq.qq.com/?_wv=1027&amp;k=B8k42CI9" target="view_window">点击前往</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
